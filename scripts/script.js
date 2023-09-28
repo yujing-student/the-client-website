3f40713663d9877aef7fa0b3f1bafb1632d49dd8
yujing-local <100352887+yujing-student@users.noreply.github.com>
2023-09-28 18:13:24 +0200

    js filter functie werkt

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,67 @@
 const body = document.querySelector('body'),
-sidebar = body.querySelector('nav'),
-toggle = body.querySelector(".toggle"),
-searchBtn = body.querySelector(".search-box"),
-modeSwitch = body.querySelector(".toggle-switch"),
-modeText = body.querySelector(".mode-text");
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
 
 
-toggle.addEventListener("click" , () =>{
+toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click" , () =>{
+searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 })
 
-modeSwitch.addEventListener("click" , () =>{
+modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
+
+    if (body.classList.contains("dark")) {
         modeText.innerText = "Light mode";
-    }else{
+    } else {
         modeText.innerText = "Dark mode";
-        
+
+    }
+});
+
+//mijn code
+// zoekfunctie op naam werkt niet geheel w3schools
+function myFunction() {
+    // Declare variables
+    let input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        // er is een variable a aangemaakt en die heeft de verzameling array li [i] waar de boeken inzitten en daar word op de ahref gelet en 0 daar begint die bij
+        txtValue = a.innerText || a.textContent;
+        // hier word de var txtvalue gemaakt waarin gekeken word of de
+        if (txtValue.toUpperCase().indexOf(filter) > 1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
+}
+
+document.querySelector("form").addEventListener('change', (event) => {
+    let filter = event.target.value;
+    let unorderedlist = document.getElementById("myUL");
+    const listbooks = unorderedlist.getElementsByTagName('listbooks');
+
+    for (let i = 0; i < listbooks.length; i++) {
+        let itemFilter = listbooks[i].dataset.filter;
+        if (itemFilter !== filter) {
+            // als itemfilter niet
+            listbooks[i].style.display = "none";
+        } else {
+            listbooks[i].style.display = "";
+        }
     }
 });
