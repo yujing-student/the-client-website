6abe0c6a467c224acf0dcc5d5ea1979f6e02197c
yujing-local <100352887+yujing-student@users.noreply.github.com>
2023-10-10 12:02:24 +0200

    aanpassing kleur

--- scripts/script.js
+++ scripts/script.js
@@ -26,12 +26,13 @@ modeSwitch.addEventListener("click", () => {
 });
 
 // hier een onlcik funciton maken
-let coll = document.querySelectorAll(".collapsible");
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        let content = document.querySelector(".collaps-open-funtion");
+        var content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
